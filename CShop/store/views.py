a28e6a227dc777597ae37f96c13c78a39573017b
Phạm Trâm Anh <93437075+tramanhpham@users.noreply.github.com>
2023-05-30 16:06:09 +0700

    almost-final

--- CShop/store/views.py
+++ CShop/store/views.py
@@ -1,11 +1,84 @@
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
-from .models import Category, Product
+from .cart import Cart
+from .forms import OrderForm
+from .models import Category, Product, Order, OrderItem
+
+def add_to_cart(request, product_id):
+    cart = Cart(request)
+    cart.add(product_id)
+
+    return redirect('cart_view')
+
+def remove_from_cart(request, product_id):
+    cart = Cart(request)
+    cart.remove(product_id)
+
+    return redirect('cart_view')
+
+def change_quantity(request, product_id):
+    action = request.GET.get('action', '')
+
+    if action:
+        quantity = 1
+
+        if action == 'decrease':
+            quantity = -1
+
+        cart = Cart(request)
+        cart.add(product_id, quantity, True)
+
+        return redirect('cart_view')
+
+def cart_view(request):
+    cart = Cart(request)
+
+    return render(request, 'store/cart_view.html', {
+        'cart': cart
+    })
+
+@login_required
+def checkout(request):
+    cart = Cart(request)
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+
+        if form.is_valid():
+            total_price = 0
+
+            for item in cart:
+                product = item['product']
+                total_price += product.price * int(item['quantity'])
+
+            order = form.save(commit=False)
+            order.created_by = request.user
+            order.paid_amount = total_price
+            order.save()
+
+            for item in cart:
+                product = item['product']
+                quantity = int(item['quantity'])
+                price = product.price * quantity
+
+                item = OrderItem.objects.create(order=order, product=product, price=price, quantity=quantity)
+
+            cart.clear()
+
+            return redirect('myaccount')
+    else:
+        form = OrderForm()
+
+    return render(request, 'store/checkout.html', {
+        'cart': cart,
+        'form': form,
+    })
 
 def search(request):
     query = request.GET.get('query', '')
-    products = Product.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))
+    products = Product.objects.filter(status=Product.ACTIVE).filter(Q(title__icontains=query) | Q(description__icontains=query))
 
     return render(request, 'store/search.html', {
         'query': query,
@@ -14,7 +87,7 @@ def search(request):
 
 def category_detail(request, slug):
     category = get_object_or_404(Category, slug=slug)
-    products = category.products.all()
+    products = category.products.filter(status=Product.ACTIVE)
 
     return render(request, 'store/category_detail.html', {
         'category': category,
@@ -22,7 +95,7 @@ def category_detail(request, slug):
     })
 
 def product_detail(request, category_slug, slug):
-    product = get_object_or_404(Product, slug=slug)
+    product = get_object_or_404(Product, slug=slug, status=Product.ACTIVE)
 
     return render(request, 'store/product_detail.html', {
         'product': product
--- CShop/userprofile/urls.py
+++ CShop/userprofile/urls.py
@@ -8,5 +8,10 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('myaccount/', views.myaccount, name='myaccount'),
+    path('my-store/', views.my_store, name='my_store'),
+    path('my-store/order-detail/<int:pk>/', views.my_store_order_detail, name='my_store_order_detail'),
+    path('my-store/add-product/', views.add_product, name='add_product'),
+    path('my-store/edit-product/<int:pk>/', views.edit_product, name='edit_product'),
+    path('my-store/delete-product/<int:pk>/', views.delete_product, name='delete_product'),
     path('vendors/<int:pk>/', views.vendor_detail, name='vendor_detail'),   
 ]
